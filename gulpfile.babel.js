1deac3aa0e99661f73a5e250bda441811a5af0c4
J.R <profotomedia@mac.com>
2016-04-29 11:13:30 -0500

    fixes BrowserSync reload issue
    BrowserSync stops reloading in ```gulp-cli@0.4.0``` 
    - updated watch task with ```.on('change')```

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -126,10 +126,10 @@ function server(done) {
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
   gulp.watch(PATHS.assets, copy);
-  gulp.watch('src/pages/**/*.html', gulp.series(pages, browser.reload));
-  gulp.watch('src/{layouts,partials}/**/*.html', gulp.series(resetPages, pages, browser.reload));
+  gulp.watch('src/pages/**/*.html').on('change', gulp.series(pages, browser.reload));
+  gulp.watch('src/{layouts,partials}/**/*.html').on('change', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/assets/scss/**/*.scss', sass);
-  gulp.watch('src/assets/js/**/*.js', gulp.series(javascript, browser.reload));
-  gulp.watch('src/assets/img/**/*', gulp.series(images, browser.reload));
-  gulp.watch('src/styleguide/**', gulp.series(styleGuide, browser.reload));
+  gulp.watch('src/assets/js/**/*.js').on('change', gulp.series(javascript, browser.reload));
+  gulp.watch('src/assets/img/**/*').on('change', gulp.series(images, browser.reload));
+  gulp.watch('src/styleguide/**').on('change', gulp.series(styleGuide, browser.reload));
 }
