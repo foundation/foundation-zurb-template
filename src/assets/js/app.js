fcf4253a003938fa0ddfc27369a5dec2fa44898c
Kevin Ball <kmball11@gmail.com>
2017-06-01 15:26:41 -0700

    First pass on webpack config for the 6.4 js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,6 @@
+import $ from 'jquery'
+import whatInput from 'what-input';
+import foundationImport from './lib/foundation-all';
+window.Foundation = foundationImport($);
+window.$ = $;
 $(document).foundation();
