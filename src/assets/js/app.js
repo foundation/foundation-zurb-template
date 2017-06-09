95ae7bcff4ed28c2902a3f4ec261b0d6be3852be
Kevin Ball <kmball11@gmail.com>
2017-06-09 16:00:12 -0700

    Update to rc3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,7 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
-import 'foundation-sites/js/entries/foundation';
+import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
