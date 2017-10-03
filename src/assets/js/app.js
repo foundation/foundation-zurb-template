cec58b7dd6cd8ad66122ac040216f49d16c54903
Patrick Huber <patrick.huber1@gmail.com>
2017-10-03 11:28:21 -0500

    Initial commit. Pages added and some initial styles. WIP

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,32 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+<<<<<<< HEAD
+
+var $window = $(window);
+
+$window.one('scroll', function() {
+  var $primaryNav = $('.nav-primary');
+  $primaryNav.addClass('visible');
+});
+
+var $parallaxSection = $('[data-parallax-bg]');
+
+$window.on('scroll', function() {
+  
+  $parallaxSection.each(function() {
+    var thisOffset = this.getBoundingClientRect()
+
+    if(thisOffset.top < document.documentElement.clientHeight && thisOffset.bottom > 0) {
+      // If in view
+      var $background = $(this).find('.background');
+      //var currentTop = $background.css('top');
+      var newTop = ((thisOffset.top / document.documentElement.clientHeight) * 50 - 30).toFixed(2) + 'px';
+      $background.css('top', newTop);
+    }
+  });
+});
+
+
+=======
+>>>>>>> e6650e6f06acbc3f9bb3456e5db817dffbcd4b75
