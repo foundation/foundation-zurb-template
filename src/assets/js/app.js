ddb33af11e3e00354c7c9ddfb43b99e6636b2785
Patrick Huber <patrick.huber1@gmail.com>
2017-10-25 17:25:36 -0500

    Removing interactive device screenshots for main projects and replacing with static screenshots; updating id's

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,9 +11,13 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
+// Custom media queries
+Foundation.Interchange.SPECIAL_QUERIES['smallRetina'] = 'only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['mediumRetina'] = 'only screen and (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx)';
 
 $(document).foundation();
 
+// Experience carousels
 $(document).ready(function ($) {
   $('.owl-carousel').owlCarousel({
     center: true,
