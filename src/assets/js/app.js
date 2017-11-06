61426143fbe4a6606786dcf49fdb209d03d289d2
RafiBomb <rafi@zurb.com>
2017-11-06 09:59:16 -0800

    adds webapp patterns and plugins

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,6 +7,8 @@ import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
+
+import 'tablesaw/dist/tablesaw.jquery';
 import libs from './lib/dependancies';
 window.libs = libs;
 
@@ -14,6 +16,7 @@ $(document).foundation();
 
 libs.AOS.init();
 
+// SVG Injector
 // Elements to inject
 var mySVGsToInject = document.querySelectorAll('img.inject-me');
 
@@ -42,3 +45,52 @@ injector.inject(
   afterAllInjectionsFinishedCallback,
   perInjectionCallback
 );
+
+// slick carousel
+$(".content-carousel").slick({
+  // normal options...
+  speed: 5000,
+	autoplay: true,
+	autoplaySpeed: 0,
+	cssEase: 'linear',
+  slidesToShow: 5,
+	slidesToScroll: 1,
+  infinite: true,
+  swipeToSlide: true,
+	centerMode: true,
+  focusOnSelect: true,
+  // the magic
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+    }, {
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+    }]
+});
+
+// tablesaw table plugin
+$(function () {
+  $(document)
+    .foundation()
+    .trigger('enhance.tablesaw');
+});
+
+var TablesawConfig = {
+  swipeHorizontalThreshold: 15
+};
+
+// app dashboard toggle
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
--- src/assets/js/lib/dependancies.js
+++ src/assets/js/lib/dependancies.js
@@ -1,10 +1,12 @@
 // custom dependencies
 import AOS from 'aos';
 import svgInjector from 'svg-injector-2';
+import slick from 'slick-carousel';
 
 const deps = {
   AOS: AOS,
-  svgInjector: svgInjector
+  svgInjector: svgInjector,
+  slickCarousel: slick
 };
 
 module.exports = deps;
