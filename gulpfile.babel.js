5e01ed5b39e77f7aad0c0aab7976179e2eac21c1
George <gtreviranus@gmail.com>
2018-02-19 13:18:03 -0800

    Adds PATHS.fonts for gulp compilation
    corrects paths and file specificity

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,7 +29,7 @@ function loadConfig() {
 
 // Build the "dist" folder by running all of the below tasks
 gulp.task('build',
- gulp.series(clean, gulp.parallel(pages, sass, fonts, javascript, images, copy), styleGuide));
+ gulp.series(clean, gulp.parallel(pages, sass, javascript, fonts, images, copy), styleGuide));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
@@ -95,11 +95,6 @@ function sass() {
     .pipe(browser.reload({ stream: true }));
 }
 
-function fonts() {
-  return gulp.src('src/assets/fonts/**/*.{ttf,woff,woff2,eot,svg}')
-    .pipe(gulp.dest(PATHS.dist + + '/assets/fonts'));
-};
-
 let webpackConfig = {
   module: {
     rules: [
@@ -127,6 +122,12 @@ function javascript() {
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.dist + '/assets/js'));
 }
+
+// Copy custom fonts to the "dist" folder
+function fonts() {
+  return gulp.src(PATHS.fonts + '.{ttf,woff,woff2,eot,svg}')
+    .pipe(gulp.dest(PATHS.dist + '/assets/fonts'));
+};
 
 // Copy images to the "dist" folder
 // In production, the images are compressed
@@ -158,7 +159,7 @@ function watch() {
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
-  gulp.watch(['src/assets/scss/**/*{tff,woff,woff2,eot,svg}.']['fonts']);
+  gulp.watch([PATHS.fonts + '.{tff,woff,woff2,eot,svg}']['fonts']);
   gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
   gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
   gulp.watch('src/styleguide/**').on('all', gulp.series(styleGuide, browser.reload));
