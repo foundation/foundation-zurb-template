ea69224e65bfaea4c4c28a195e6d3b271b143776
teddywsi <49923665+teddywsi@users.noreply.github.com>
2019-05-09 16:55:11 -0400

    Fix typos

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 import 'what-input';
 
-// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
-// to the top of the file so if we used`import` to import Foundation,
+// Foundation JS relies on a global variable. In ES6, all imports are hoisted
+// to the top of the file so if we used `import` to import Foundation,
 // it would execute earlier than we have assigned the global variable.
 // This is why we have to use CommonJS require() here since it doesn't
 // have the hoisting behavior.
